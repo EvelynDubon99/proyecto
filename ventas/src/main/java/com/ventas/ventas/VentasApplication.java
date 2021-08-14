8b0f3730727ff69119b38bfb9b26b35420029b7c
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-08-14 14:06:38 -0600

    fase 1

---
+++ ventas/src/main/java/com/ventas/ventas/VentasApplication.java
@@ -0,0 +1,18 @@
+package com.ventas.ventas;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+  * <p>Clase donde se corre el programa, utilizada por el springframework </p>
+ */
+@SpringBootApplication
+public class VentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasApplication.class, args);
+	}
+
+}
