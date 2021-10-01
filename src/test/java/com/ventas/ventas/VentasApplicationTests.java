50916b2d2c194adad5c0f57b991aa6a2838ae7c1
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-10-01 14:36:35 -0600

    aaa

--- src/test/java/com/ventas/ventas/VentasApplicationTests.java
+++ src/test/java/com/ventas/ventas/VentasApplicationTests.java
@@ -1,16 +1,47 @@
 package com.ventas.ventas;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 class VentasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
+	public Telefono tel = new Telefono();
+
+	@Test
+	void Testgetysetters() throws Exception {
+
+		tel.setNombret("NombrePrueba");
+		tel.setAlmacenamiento(80);
+		tel.setCodigo_modelo("a7");
+		tel.setColor("Rojo");
+		tel.setExistencia(10);
+		tel.setDescripcion("Descripcion de prueba");
+		tel.setNombre_marca("Marca");
+		tel.setRam(8);
+		tel.setPrecio_lista(85);
+		tel.setProcesador("Snap-prueba");
+		tel.setId_dispositivo(1);
+		tel.setNumero_serie("55AA");
+		tel.setNumero_cores(8);
+
+		assertSame("NombrePrueba", tel.getNombret());
+		assertSame(80, tel.getAlmacenamiento());
+		assertSame("a7", tel.getCodigo_modelo());
+		assertSame("Rojo", tel.getColor());
+		assertSame(10, tel.getExistencia());
+		assertSame("Descripcion de prueba", tel.getDescripcion());
+		assertSame("Marca", tel.getNombre_marca());
+		assertSame(8, tel.getRam());
+		assertEquals(85.0, tel.getPrecio_lista());
+		assertSame("Snap-prueba", tel.getProcesador());
+		assertSame(1, tel.getId_dispositivo());
+		assertSame("55AA", tel.getNumero_serie());
+		assertSame(8, tel.getNumero_cores());
+
+	}
 }
