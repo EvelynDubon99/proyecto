aa7073f68a86e130d2cb3b6bd5f8c11e6c31042b
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-10-21 14:06:43 -0600

    arreglo

---
+++ src/test/java/com/ventas/ventas/MarcaSetetrsGettersTest.java
@@ -0,0 +1,34 @@
+package com.ventas.ventas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@AutoConfigureMockMvc
+public class MarcaSetetrsGettersTest extends VentasApplicationTests {
+
+    public Marca marca = new Marca();
+
+    @Test
+    public void Testnombre_marca() throws Exception {
+
+        marca.setNombre_marca("NombrePrueba");
+        assertEquals("NombrePrueba", marca.getNombre_marca());
+        assertNotNull(marca.getNombre_marca());
+
+    }
+
+    @Test
+    public void Testid_marca() throws Exception {
+
+        marca.setId_marca(1);
+        assertEquals(1, marca.getId_marca());
+        assertNotNull(marca.getId_marca());
+
+    }
+}
